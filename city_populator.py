63f501eda5db8a868f7b579ed65204fc9fc509b5
suripoori <suraj.srinivasa@gmail.com>
2016-03-13 19:38:12 -0400

    Moved ratings and reviews to the associations

--- city_populator.py
+++ city_populator.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from trip_planner.models import Restaurant, Base, City, User, Hotel, Attraction, \
-    UserRestaurantAssociation, UserAttractionAssociation, UserHotelAssociation
+    UserRestaurantAssociation, UserAttractionAssociation, UserHotelAssociation, UserCityAssociation
 
 engine = create_engine('sqlite:///tripplanner.db')
 # Bind the engine to the metadata of the Base class so that the
@@ -40,64 +40,91 @@ session.commit()
 # Create a few cities
 City1 = City(city_name="New York", state="New York", country="USA")
 session.add(City1)
+a1 = UserCityAssociation(trip_log="New York, so many places to eat! So many things to see! Loved the trip!")
+a1.user = User1
+a2 = UserCityAssociation(trip_log="We went to Grimaldi's for lunch during our brief visit")
+a2.user = User2
+a3 = UserCityAssociation(trip_log="I went there just for the Indian food")
+a3.user = User3
+City1.travelers.append(a1)
+City1.travelers.append(a2)
+City1.travelers.append(a3)
 session.commit()
 
 City2 = City(city_name="Boston", state="Massachussetts", country="USA")
 session.add(City2)
+a4 = UserCityAssociation(trip_log="Lot of history in this place")
+a4.user = User1
+a5 = UserCityAssociation(trip_log="Not many Indian restaurants here")
+a5.user = User3
+City2.travelers.append(a4)
+City2.travelers.append(a5)
 session.commit()
 
 City3 = City(city_name="Fort Lauderdale", state="Florida", country="USA")
 session.add(City3)
+a6 = UserCityAssociation(trip_log="Ah the beaches")
+a6.user = User1
+a7 = UserCityAssociation(trip_log="Too many old people")
+a7.user = User2
+City3.travelers.append(a6)
+City3.travelers.append(a7)
 session.commit()
 
 City4 = City(city_name="Bangalore", state="Karnataka", country="India")
 session.add(City4)
+a8 = UserCityAssociation(trip_log="Tech crowd")
+a8.user = User2
+a9 = UserCityAssociation(trip_log="South Indian food!")
+a9.user = User3
+City4.travelers.append(a8)
+City4.travelers.append(a9)
 session.commit()
 
 # Create a few Restaurants
 Restaurant1 = Restaurant(restaurant_name="Grimaldi's Pizza", city_id=1)
 session.add(Restaurant1)
-assoc1 = UserRestaurantAssociation()
+assoc1 = UserRestaurantAssociation(review="NY style pizzas!", rating=5)
 assoc1.user = User1
 Restaurant1.customers.append(assoc1)
-assoc2 = UserRestaurantAssociation()
+assoc2 = UserRestaurantAssociation(review="Didn't care much about the food, was in a rush, slow and crowded", rating=2)
 assoc2.user = User2
 Restaurant1.customers.append(assoc2)
 session.commit()
 
 Restaurant2 = Restaurant(restaurant_name="Saravana Bhavan", city_id=1)
 session.add(Restaurant2)
-assoc3 = UserRestaurantAssociation()
+assoc3 = UserRestaurantAssociation(review="I love south Indian food", rating=5)
 assoc3.user = User3
 Restaurant2.customers.append(assoc3)
 session.commit()
 
 Restaurant3 = Restaurant(restaurant_name="Kashmir", city_id=2)
 session.add(Restaurant3)
-assoc4 = UserRestaurantAssociation()
+assoc4 = UserRestaurantAssociation(review="Good Indian food", rating=4)
 assoc4.user = User3
 Restaurant3.customers.append(assoc4)
 session.commit()
 
 Restaurant4 = Restaurant(restaurant_name="Fugakyu", city_id=2)
 session.add(Restaurant4)
-assoc5 = UserRestaurantAssociation()
+assoc5 = UserRestaurantAssociation(review="I like sushi!", rating=5)
 assoc5.user = User1
 Restaurant4.customers.append(assoc5)
 session.commit()
 
 Restaurant5 = Restaurant(restaurant_name="It's all greek to me", city_id=3)
 session.add(Restaurant5)
-assoc6 = UserRestaurantAssociation()
+assoc6 = UserRestaurantAssociation(review="Good for a quick and cheap bite", rating=4)
 assoc6.user = User1
 Restaurant5.customers.append(assoc6)
 session.commit()
 
 Restaurant6 = Restaurant(restaurant_name="Thai Spice", city_id=3)
 session.add(Restaurant6)
-assoc7 = UserRestaurantAssociation()
+assoc7 = UserRestaurantAssociation(review="Great ambience, good food", rating=5)
 assoc7.user = User1
-assoc8 = UserRestaurantAssociation()
+assoc8 = UserRestaurantAssociation(review="Went for dinner, too expensive", rating=2)
 assoc8.user = User2
 Restaurant6.customers.append(assoc7)
 Restaurant6.customers.append(assoc8)
@@ -105,16 +132,16 @@ session.commit()
 
 Restaurant7 = Restaurant(restaurant_name="Indraprastha", city_id=4)
 session.add(Restaurant7)
-assoc9 = UserRestaurantAssociation()
+assoc9 = UserRestaurantAssociation(review="I love south Indian food!", rating=5)
 assoc9.user = User3
 Restaurant7.customers.append(assoc9)
 session.commit()
 
 Restaurant8 = Restaurant(restaurant_name="CTR", city_id=4)
 session.add(Restaurant8)
-assoc10 = UserRestaurantAssociation()
+assoc10 = UserRestaurantAssociation(review="Took too much time to get a table", rating=2)
 assoc10.user = User2
-assoc11 = UserRestaurantAssociation()
+assoc11 = UserRestaurantAssociation(review="Best dosa in town!", rating=5)
 assoc11.user = User3
 Restaurant8.customers.append(assoc10)
 Restaurant8.customers.append(assoc11)
--- trip_planner/models.py
+++ trip_planner/models.py
@@ -3,50 +3,54 @@ __author__ = 'Suraj'
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine, Table, CheckConstraint
+from sqlalchemy import CheckConstraint
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
-class UserCityAssociation(Base):
 
+class UserCityAssociation(Base):
     __tablename__ = 'user_city_association'
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     city_id = Column(Integer, ForeignKey('city.id'), primary_key=True)
     user = relationship("User", back_populates="cities")
     city = relationship("City", back_populates="travelers")
+    trip_log = Column(String(400))
 
 
 class UserRestaurantAssociation(Base):
-
     __tablename__ = 'user_restaurant_association'
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'), primary_key=True)
     user = relationship("User", back_populates="restaurants")
     restaurant = relationship("Restaurant", back_populates="customers")
+    review = Column(String(400))
+    rating = Column(Integer, CheckConstraint('rating >= 0 AND rating <=5'))
 
 
 class UserHotelAssociation(Base):
-
     __tablename__ = 'user_hotel_association'
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     hotel_id = Column(Integer, ForeignKey('hotel.id'), primary_key=True)
     user = relationship("User", back_populates="hotels")
     hotel = relationship("Hotel", back_populates="guests")
+    review = Column(String(400))
+    rating = Column(Integer, CheckConstraint('rating >= 0 AND rating <=5'))
 
 
 class UserAttractionAssociation(Base):
-
     __tablename__ = 'user_attraction_association'
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     attraction_id = Column(Integer, ForeignKey('attraction.id'), primary_key=True)
     user = relationship("User", back_populates="attractions")
     attraction = relationship("Attraction", back_populates="visitors")
+    review = Column(String(400))
+    rating = Column(Integer, CheckConstraint('rating >= 0 AND rating <=5'))
 
 
 class User(Base):
-
     __tablename__ = 'user'
 
     user_name = Column(String(80), nullable=False)
@@ -61,19 +65,18 @@ class User(Base):
     @property
     def serialize(self):
         return {
-            'user_name' : self.user_name,
-            'id' : self.id,
-            'email' : self.email,
-            'picture' : self.picture,
-            'cities' : self.cities,
-            'restaurants' : self.restaurants,
-            'hotels' : self.hotels,
-            'attractions' : self.attractions
+            'user_name': self.user_name,
+            'id': self.id,
+            'email': self.email,
+            'picture': self.picture,
+            'cities': self.cities,
+            'restaurants': self.restaurants,
+            'hotels': self.hotels,
+            'attractions': self.attractions
         }
 
 
 class City(Base):
-
     __tablename__ = 'city'
     id = Column(Integer, primary_key=True)
     city_name = Column(String(80), nullable=False)
@@ -87,16 +90,15 @@ class City(Base):
     @property
     def serialize(self):
         return {
-            'city_name' : self.city_name,
-            'id' : self.id,
-            'state' : self.state,
-            'country' : self.country,
-            'users' : self.users
+            'city_name': self.city_name,
+            'id': self.id,
+            'state': self.state,
+            'country': self.country,
+            'travelers': self.travelers
         }
 
 
 class Restaurant(Base):
-
     __tablename__ = 'restaurant'
 
     restaurant_name = Column(String(80), nullable=False)
@@ -104,23 +106,28 @@ class Restaurant(Base):
     customers = relationship("UserRestaurantAssociation", back_populates="restaurant")
     city_id = Column(Integer, ForeignKey('city.id'))
     city = relationship("City")
-    rating = Column(Integer, CheckConstraint('rating >= 0 AND rating <=5'))
+
+    @property
+    def rating(self):
+        return session.query(func.avg(UserRestaurantAssociation.rating)).filter_by(restaurant_id=self.id).one()[0]
+
+    @property
+    def number_of_customers(self):
+        return len(self.customers)
 
     # We added this serialize function to be able to send JSON objects in a serializable format
     @property
     def serialize(self):
-
-       return {
-           'restaurant_name' : self.restaurant_name,
-           'id' : self.id,
-           'customers' : self.customers,
-           'city_id' : self.city_id,
-           'rating' : self.rating
-       }
+        return {
+            'restaurant_name': self.restaurant_name,
+            'id': self.id,
+            'number_of_customers': self.number_of_customers,
+            'city_id': self.city_id,
+            'rating': self.rating
+        }
 
 
 class Hotel(Base):
-
     __tablename__ = 'hotel'
 
     hotel_name = Column(String(80), nullable=False)
@@ -128,40 +135,35 @@ class Hotel(Base):
     city_id = Column(Integer, ForeignKey('city.id'))
     city = relationship("City")
     guests = relationship("UserHotelAssociation", back_populates="hotel")
-    rating = Column(Integer, CheckConstraint('rating >= 0 AND rating <= 5'))
 
     @property
     def serialize(self):
-
         return {
-            'hotel_name' : self.hotel_name,
-            'id' : self.id,
-            'guests' : self.guests,
-            'rating' : self.rating,
-            'city_id' : self.city_id
+            'hotel_name': self.hotel_name,
+            'id': self.id,
+            'guests': self.guests,
+            'rating': self.rating,
+            'city_id': self.city_id
         }
 
 
 class Attraction(Base):
-
     __tablename__ = 'attraction'
 
     id = Column(Integer, primary_key=True)
     attraction_name = Column(String(80), nullable=False)
-    rating = Column(Integer, CheckConstraint('rating >= 0 AND rating <= 5'))
     city_id = Column(Integer, ForeignKey('city.id'))
     city = relationship("City")
     visitors = relationship("UserAttractionAssociation", back_populates="attraction")
 
     @property
     def serialize(self):
-
         return {
-            'attraction_name' : self.attraction_name,
-            'id' : self.id,
-            'rating' : self.rating,
-            'visitors' : self.visitors,
-            'city_id' : self.city_id
+            'attraction_name': self.attraction_name,
+            'id': self.id,
+            'rating': self.rating,
+            'visitors': self.visitors,
+            'city_id': self.city_id
         }
 
 
